37fe1abca8118c486d579be8f572c01fce342b5d
Gustavo-yggdrasil <gustavo110325@gmail.com>
2025-03-03 18:23:18 -0300

    estudo 02
    estudos 02

---
+++ exercicos/ex015/script.js
@@ -0,0 +1,7 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var msg = window.document.getElementById('msg')
+    var igm = window.document.getElementById('img')
+
+    msg.innerHTML = `Agora e Horas ${hora} : ${min}`
+}
