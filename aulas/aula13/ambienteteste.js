56f0474cd7faee393978e054f9efe21cc1d051a5
Gustavo-yggdrasil <gustavo110325@gmail.com>
2025-03-05 22:26:12 -0300

    estudos

---
+++ aulas/aula13/ambienteteste.js
@@ -0,0 +1,20 @@
+/*
+var c = 1
+
+while (c <= 6){
+
+    console.log(`Passo ${c}`)// Teste logico no inicio antes de iniciar o bloco
+
+    c++
+
+}*/
+
+var c = 1
+
+do{
+
+    console.log(`Passo ${c}`)// Teste logico no final do bloco apos ter cido realizado a ação do bloco
+
+    c++
+
+}while (c <= 6)
